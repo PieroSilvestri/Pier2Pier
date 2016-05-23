3117b222d91a4cb8df68026e78b07f29dc31e890
pierosavi <piero.savi@hotmail.it>
2016-05-23 20:21:33 +0200

    Swipe to do stuff, da rivedere

--- PostNote1/app/src/main/java/com/example/piero/postnote1/AllFragment.java
+++ PostNote1/app/src/main/java/com/example/piero/postnote1/AllFragment.java
@@ -6,12 +6,20 @@ import android.app.Fragment;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.LayoutInflater;
@@ -24,6 +32,7 @@ import android.view.ViewGroup;
 import android.widget.ListAdapter;
 import android.widget.Toast;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,6 +51,8 @@ public class AllFragment extends Fragment implements SearchView.OnQueryTextListe
     public String TAGCICLO = "CICLODIVITA";
     private String myID = "";
     Dettaglio dettaglio;
+    private Paint p = new Paint();
+    private View view;
 
 
     public AllFragment() {
@@ -165,9 +176,11 @@ public class AllFragment extends Fragment implements SearchView.OnQueryTextListe
 
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), LinearLayoutManager.VERTICAL));
-        recyclerView.addItemDecoration(new VerticalSpaceItemDecoration(25));
+        //recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), LinearLayoutManager.VERTICAL));
+        //recyclerView.addItemDecoration(new VerticalSpaceItemDecoration(25));
         recyclerView.setAdapter(mAdapter);
+
+        initSwipe();
 
         recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getActivity(), recyclerView, new ClickListener() {
             @Override
@@ -183,7 +196,7 @@ public class AllFragment extends Fragment implements SearchView.OnQueryTextListe
             }
 
             @Override
-            public void onLongClick(View view, final int position) {
+            public void onLongClick(View view, final int position) {/*
                 AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
                 ListAdapter adapter = new ArrayAdapterWithIcon(getActivity(), scelte, icons);
                 builder.setTitle("Cosa Desideri fare?")
@@ -198,13 +211,12 @@ public class AllFragment extends Fragment implements SearchView.OnQueryTextListe
                                                 + temPostItem.getTesto());
                                         sendIntent.setType("text/*");
                                         Log.d("primo", "primo");
-                                        /*if (temPostItem.getPosizioneAudio() != null) {
+                                        if (temPostItem.getPosizioneAudio() != null) {
                                             sendIntent.setType("audio/*");
                                             sendIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(new File(temPostItem.getPosizioneAudio())));
                                             Log.d("secondo", "secondo");
-                                        }*/
-
-                                       /* if (temPostItem.getcreationDate() != null && temPostItem.getcreationDate() != "") {
+                                        }
+                                        if (temPostItem.getcreationDate() != null && temPostItem.getcreationDate() != "") {
                                             Uri pictureUri = Uri.parse(temPostItem.getcreationDate().replaceAll("/", "").replaceAll(":", "").replaceAll(" ", "").toString());
                                             sendIntent.setType("image/*");
                                             final File photoFile = new File(temPostItem.getcreationDate().replaceAll("/", "").replaceAll(":", "").replaceAll(" ", "").toString());
@@ -213,7 +225,7 @@ public class AllFragment extends Fragment implements SearchView.OnQueryTextListe
                                             sendIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(photoFile));
 
                                             Log.d("terzo", "terzo");
-                                        }*/
+                                        }
 
                                         Log.d("fine", "fine");
                                         startActivity(Intent.createChooser(sendIntent, "Scegli"));
@@ -252,9 +264,75 @@ public class AllFragment extends Fragment implements SearchView.OnQueryTextListe
                 builder.create();
                 builder.show();
 
-            }
+            */}
         }));
         return rootView;
+    }
+
+    private void initSwipe(){
+        ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                int position = viewHolder.getAdapterPosition();
+
+                if (direction == ItemTouchHelper.LEFT){
+                    myID = String.valueOf(filteredModelList.get(position).getId());
+                    DeleteData(myID);
+                    allList.remove(position);
+                    filteredModelList = allList;
+                    UpdateList();
+                } else {
+                    Intent sendIntent = new Intent();
+                    PostItem temPostItem = filteredModelList.get(position);
+                    sendIntent.setAction(Intent.ACTION_SEND);
+                    sendIntent.putExtra(Intent.EXTRA_TEXT, temPostItem.getTitolo().toUpperCase() + "\n" + temPostItem.getTesto());
+                    sendIntent.setType("text/*");
+                    startActivity(Intent.createChooser(sendIntent, "Scegli"));
+                }
+            }
+
+            @Override
+            public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+
+                Bitmap icon;
+                if(actionState == ItemTouchHelper.ACTION_STATE_SWIPE){
+
+                    View itemView = viewHolder.itemView;
+                    float height = (float) itemView.getBottom() - (float) itemView.getTop();
+                    float width = height / 3;
+
+                    if(dX > 0){
+                        p.setColor(Color.parseColor("#388E3C"));
+                        RectF background = new RectF((float) itemView.getLeft(), (float) itemView.getTop(), dX,(float) itemView.getBottom());
+                        c.drawRect(background,p);
+                        icon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_done_all_black_48dp);
+                        RectF icon_dest = new RectF((float) itemView.getLeft() + width ,(float) itemView.getTop() + width,(float) itemView.getLeft()+ 2*width,(float)itemView.getBottom() - width);
+                        c.drawBitmap(icon,null,icon_dest,p);
+                    } else {
+                        p.setColor(Color.parseColor("#D32F2F"));
+                        RectF background = new RectF((float) itemView.getRight() + dX, (float) itemView.getTop(),(float) itemView.getRight(), (float) itemView.getBottom());
+                        c.drawRect(background,p);
+                        icon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_clear_black_24dp);
+                        RectF icon_dest = new RectF((float) itemView.getRight() - 2*width ,(float) itemView.getTop() + width,(float) itemView.getRight() - width,(float)itemView.getBottom() - width);
+                        c.drawBitmap(icon,null,icon_dest,p);
+                    }
+                }
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+            }
+        };
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
+        itemTouchHelper.attachToRecyclerView(recyclerView);
+    }
+    private void removeView(){
+        if(view.getParent()!=null) {
+            ((ViewGroup) view.getParent()).removeView(view);
+        }
     }
 
     @Override
