704a95922f94e1daaa2c60cdc15cbf3317f854e4
munsra <piero.silvestri93@gmail.com>
2016-06-01 10:38:43 +0200

    Cancellazione robe....
    Da controllare

--- PostNote1/app/src/main/java/com/example/piero/postnote1/MainActivity.java
+++ PostNote1/app/src/main/java/com/example/piero/postnote1/MainActivity.java
@@ -97,7 +97,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         myDB1 = new DatabaseHelper(this);
 
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
